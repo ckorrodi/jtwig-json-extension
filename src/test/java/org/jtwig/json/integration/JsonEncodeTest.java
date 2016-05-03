107c9067b4286582ab98781eec38f7c717b0f6da
Joao De Melo <jmelo@lyncode.com>
2016-05-03 18:26:49 +0100

    Adding API to ease configuration

--- src/test/java/org/jtwig/json/integration/JsonEncodeTest.java
+++ src/test/java/org/jtwig/json/integration/JsonEncodeTest.java
@@ -2,21 +2,23 @@ package org.jtwig.json.integration;
 
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
+import org.jtwig.environment.EnvironmentConfigurationBuilder;
 import org.jtwig.json.JsonExtension;
-import org.jtwig.json.configuration.DefaultJsonMapperProviderConfiguration;
 import org.jtwig.json.configuration.JsonMapperProviderConfigurationBuilder;
 import org.junit.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsNot.not;
-import static org.jtwig.environment.EnvironmentConfigurationBuilder.configuration;
 
 public class JsonEncodeTest {
     @Test
     public void name() throws Exception {
-        String result = JtwigTemplate.inlineTemplate("{{ variable | json_encode }}", configuration()
-                .extensions().add(new JsonExtension(new JsonMapperProviderConfigurationBuilder(new DefaultJsonMapperProviderConfiguration()).build())).and()
+        String result = JtwigTemplate.inlineTemplate("{{ variable | json_encode }}", EnvironmentConfigurationBuilder.configuration()
+                .extensions().add(new JsonExtension(JsonMapperProviderConfigurationBuilder
+                        .jsonConfiguration()
+                        .build()))
+                .and()
                 .build())
                 .render(JtwigModel.newModel().with("variable", new TestEntity().setName("hello")));
 
